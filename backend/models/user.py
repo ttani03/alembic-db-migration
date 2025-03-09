a54836ec70b6aae18ae936aa541bce881e99e959
ttani03 <68139726+ttani03@users.noreply.github.com>
2025-03-10 03:59:48 +0900

    add email

--- backend/models/user.py
+++ backend/models/user.py
@@ -9,7 +9,7 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String(50), unique=True, index=True)
-    # email = Column(String(50), unique=True, index=True)
+    email = Column(String(50), unique=True, index=True)
     # hashed_password = Column(String(100))
     # is_active = Column(Boolean, default=True)
     created_at = Column(DateTime, default=datetime.now)
